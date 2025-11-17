4954cbe6c292a75de61e351f4b4fdce7fd4215d0
bob <brilliantcontract@yahoo.com>
2025-11-17 17:34:02 +0000

    Initialize project

---
+++ src/main/java/bc/bfi/maps_scraper_cid/BfiGoogleMapsScraperSerperCid.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bc.bfi.maps_scraper_cid;
+
+/**
+ *
+ * @author bob
+ */
+public class BfiGoogleMapsScraperSerperCid {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
