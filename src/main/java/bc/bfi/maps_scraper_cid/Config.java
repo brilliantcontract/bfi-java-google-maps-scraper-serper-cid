ca6b58c46302b332ade26d8b1a4d4ac2cb03dcb7
bob <brilliantcontract@yahoo.com>
2025-11-17 17:44:24 +0000

    Add Config and cids files.

---
+++ src/main/java/bc/bfi/maps_scraper_cid/Config.java
@@ -0,0 +1,6 @@
+package bc.bfi.maps_scraper_cid;
+
+public class Config {
+    private static final String API_KEY = "0c833bbdac7eb41ed54b9b786e375ab15492c440";
+    private static final String PLACES_URL = "https://google.serper.dev/maps";
+}
